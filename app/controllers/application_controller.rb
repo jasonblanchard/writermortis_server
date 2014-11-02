9f2c3cb48951f97ca304c7b40d4ccf3f95ceb84b
Jason Blanchard <jas.blanchard@gmail.com>
2014-11-02 09:38:27 -0500

    Using headers to find email and token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,17 @@ class ApplicationController < ActionController::API
   after_filter :cors_set_access_control_headers, :if => :in_dev
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  def handle_options_request
+    head(:ok) if request.request_method == "OPTIONS"
+  end
+
   private
 
   def authenticate_user_from_token!
-    user_token = params[:user_token].presence
-    user_email = params[:user_email].presence
+    auth_header = request.headers["Authorization"] || ''
+    # TODO Can we remove check for token in params?
+    user_token = auth_header[/token="([\w\d-]+)/,1] || params[:user_token].presence
+    user_email = auth_header[/email="(.+)"/,1] || params[:user_email].presence
     user = user_email && User.find_by_email(user_email)
 
     if user && Devise.secure_compare(user.authentication_token, user_token)
@@ -29,9 +35,10 @@ class ApplicationController < ActionController::API
 
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin']      = '*'
-    headers['Access-Control-Allow-Methods']     = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Methods']     = 'POST, GET, PUT, DELETE, OPTIONS'
     headers['Access-Control-Max-Age']           = '1728000'
     headers['Access-Control-Allow-Credentials'] = 'true'
+    headers['Access-Control-Allow-Headers']     = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 
   def in_dev

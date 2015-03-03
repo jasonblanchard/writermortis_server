dad875828d1f9a7b8aced05e0ae5e94410b54681
Jason Blanchard <jas.blanchard@gmail.com>
2015-03-03 07:29:22 -0500

    Fixed n+1 issue

--- app/controllers/api/v1/stories_controller.rb
+++ app/controllers/api/v1/stories_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::StoriesController < ApplicationController
   before_action :get_story, :only => [:show, :update, :destroy]
 
   def index
-    @stories = Story.sorted
+    @stories = Story.sorted.includes(:user)
     render :json => @stories
   end
 

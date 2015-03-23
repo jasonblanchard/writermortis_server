c8a3565b027469a256a718dc615953bbc1205da1
Jason Blanchard <jas.blanchard@gmail.com>
2015-03-23 08:09:36 -0400

    Added updated at and created at to story object

--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -3,7 +3,7 @@ class StorySerializer < ActiveModel::Serializer
   has_many :pieces
   has_one :user
 
-  attributes :id, :title, :total_pieces, :max_sentences
+  attributes :id, :title, :total_pieces, :max_sentences, :created_at, :updated_at
 
   def pieces
     object.pieces.includes(:user)

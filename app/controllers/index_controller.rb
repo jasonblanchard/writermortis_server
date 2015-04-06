7931d711ad667712427bc16c9317cb953de4ca50
Jason Blanchard <jas.blanchard@gmail.com>
2015-04-06 08:42:55 -0400

    Added redis bootstrap

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,15 @@
 class IndexController < ApplicationController
+
   def index
+    index_key = params[:index_key]
+    render :html => bootstrap_index(index_key)
   end
+
+  private
+
+  def bootstrap_index(index_key)
+    index_key ||= $redis.get('writermortis:current')
+    $redis.get(index_key)
+  end
+
 end

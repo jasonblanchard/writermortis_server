ad3d6579d8c37098c9f96e639eb45bc129da96ff
Jason Blanchard <jas.blanchard@gmail.com>
2015-04-10 08:23:22 -0400

    Added catchall route to forward to client app

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
 
   root to: 'index#index'
 
+  get '*unmatched_route', :to => 'index#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
